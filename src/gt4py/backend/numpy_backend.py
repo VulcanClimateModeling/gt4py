5f311a1fd74662d5cd6ce38cad08e21a39932c75
Eddie Davis <eddied@vulcan.com>
2021-06-18 12:28:35 -0700

    Fix numpy backend

--- src/gt4py/backend/numpy_backend.py
+++ src/gt4py/backend/numpy_backend.py
@@ -226,9 +226,6 @@ class NumPySourceGenerator(PythonSourceGenerator):
         intervals = kwargs.get("intervals", None)
         assert node.name in self.block_info.accessors
 
-        extent = self.block_info.extent
-        lower_extent = list(extent.lower_indices)
-        upper_extent = list(extent.upper_indices)
         parallel_axes_names = [
             axis
             for axis in self.impl_node.fields[node.name].axes
@@ -236,11 +233,8 @@ class NumPySourceGenerator(PythonSourceGenerator):
         ]
         parallel_axes_dims = [self.impl_node.domain.index(axis) for axis in parallel_axes_names]
 
-        for d, ax in enumerate(parallel_axes_names):
-            idx = node.offset.get(ax, 0)
-            if idx:
-                lower_extent[d] += idx
-                upper_extent[d] += idx
+        lower_indices = self.block_info.extent.lower_indices
+        upper_indices = self.block_info.extent.upper_indices
 
         index = []
         for fd, d in enumerate(parallel_axes_dims):
